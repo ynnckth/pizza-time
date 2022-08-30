5a628cb429b407545ff66a721936990ae1fa43f1
ynnckth <yannickstreit@hotmail.com>
2022-08-30 12:03:36 +0800

    Remove CRA boilerplate code; introduce MUI component library

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+interface Props {
+  title: string;
+}
+
+const Header: React.FC<Props> = ({ title }) => {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '45px',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+      }}
+    >
+      <Typography>{title}</Typography>
+    </Box>
+  );
+};
+export default Header;
