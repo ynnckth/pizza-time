736e8f48f83709e9b2d97f0ffa5e47bfa53b368d
ynnckth <yannickstreit@hotmail.com>
2022-09-08 15:35:47 +0800

    Add insecure CORS config for now to allow all origins; add TODO to introduce proper web security with Spring Security

---
+++ order-service/src/main/java/com/ynnckth/pizzatime/orderservice/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ynnckth.pizzatime.orderservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// TODO: introduce Spring Security and handle with web security config instead
+//  https://www.baeldung.com/spring-cors#cors-with-spring-security
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
