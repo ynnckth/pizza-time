2c8c9fd6a68c7239299c047e4cbd707ae08b64bd
ynnckth <yannickstreit@hotmail.com>
2022-09-09 11:05:59 +0800

    Include version information in deployed frontend artifact including build version and commit hash

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box } from '@mui/material';
 import Header from './components/Header/Header';
 import Marketplace from './pages/Marketplace/Marketplace';
@@ -7,6 +7,13 @@ import Checkout from './pages/Checkout/Checkout';
 import PastOrders from './pages/PastOrders/PastOrders';
 
 const App = () => {
+  useEffect(() => {
+    console.group('Version Information');
+    console.log('Build version: ', process.env.REACT_APP_BUILD_VERSION);
+    console.log('Commit hash: ', process.env.REACT_APP_COMMIT_HASH);
+    console.groupEnd();
+  }, []);
+
   return (
     <Box sx={{ width: '100vw', height: '100vh' }}>
       <Header title={'🍕 Pizza Time!'} />
